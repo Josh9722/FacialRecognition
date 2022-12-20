441c31ea8c595b353a08f87f1259b725150692e0
Josh9722 <wellsy9722@gmail.com>
2022-12-20 19:24:51 +1100

    Complete setup for haar frontalface

---
+++ DataPath.cs
@@ -0,0 +1,7 @@
+public static class DataPath { 
+    // Images
+    public const string Yalta = "Data/Images/Yalta.jpg";
+
+    // Cascades
+    public const string HaarCascade = "Data/Cascades/haarcascade_frontalface_default.xml"; 
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Formats.Jpeg;
+using OpenCvSharp;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+
+
+
+namespace FacialRecognition
+{
+    public static class ImageProcessing
+    {
+        public static void Main(string[] args)
+        {
+            DetectFace face = new DetectFace();
+            face.RunTest();  
+            
+        }
+        
+    }
+}
