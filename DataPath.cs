31186ad11787ed3777aaba169c015a26e9236c47
Josh9722 <wellsy9722@gmail.com>
2022-12-21 18:09:07 +1100

    Added functionaliy for using multiple cascades

--- DataPath.cs
+++ DataPath.cs
@@ -1,7 +1,16 @@
 public static class DataPath { 
     // Images
-    public const string Yalta = "Data/Images/Yalta.jpg";
+    public const string ImagesCheck = "Data/Images/ImagesCheck/"; // Check all images in folder
+    public const string ImagesOutput = "Data/Images/ImagesOutput/"; // Output folder for images
+    public const string PeopleGrid = "Data/Images/PeopleGrid.jpg";
+    
 
     // Cascades
+    public const string CascadeFolder = "Data/Cascades/"; // Folder with all cascades
     public const string HaarCascade = "Data/Cascades/haarcascade_frontalface_default.xml"; 
+    public const string HaarCascadeAlt = "Data/Cascades/haarcascade_frontalface_alt.xml";
+
+    public static string[] AllCascades() { 
+        return Directory.GetFiles(CascadeFolder, "*.xml", SearchOption.AllDirectories);
+    }
 }
--- DetectFace.cs
+++ DetectFace.cs
@@ -7,30 +7,71 @@ class DetectFace {
     public void RunTest()
     {
         // Load the cascades
-        using var haarCascade = new CascadeClassifier(DataPath.HaarCascade);
+        string[] cascadePaths = DataPath.AllCascades();
+        CascadeClassifier[] allCascades = new CascadeClassifier[cascadePaths.Length];
+        for(int i = 0; i < cascadePaths.Length; i++) {
+            allCascades[i] = new CascadeClassifier(cascadePaths[i]);
+        }
 
         // Detect faces
-        Mat haarResult = FindFace(haarCascade);
-        
+        Mat haarResult = FindFace(allCascades);
         Cv2.ImShow("Faces by Haar", haarResult);
         Cv2.WaitKey(0);
         Cv2.DestroyAllWindows();
     }
 
+    private Rect[] GetFaces(params CascadeClassifier[] ListOfCascades)
+    {
+        List<Rect> faces = new List<Rect>();
+        CascadeClassifier[] cascades = ListOfCascades;
+        using Mat src = new Mat(DataPath.PeopleGrid, ImreadModes.Color);
+        using Mat gray = new Mat();
 
-    private Mat FindFace(CascadeClassifier cascade)
+
+        foreach (CascadeClassifier cascade in cascades)
+        {
+            Rect[] detectedFaces = cascade.DetectMultiScale(new Mat(DataPath.PeopleGrid, ImreadModes.Color), 1.08, 2, HaarDetectionTypes.ScaleImage);
+
+            foreach (Rect detectedFace in detectedFaces) {
+                if (!faces.Any(face => face.IntersectsWith(detectedFace) || face.Contains(detectedFace))) { 
+                    faces.Add(detectedFace);
+                }
+            }
+        }
+        Console.WriteLine(faces.Count);
+
+        return faces.ToArray();
+    }
+
+
+    private void SaveFacesInNewImages(params CascadeClassifier[] cascade) { 
+        Rect[] faces = GetFaces(cascade);
+        Console.WriteLine(faces.Length); // DEBUG
+        using var src = new Mat(DataPath.PeopleGrid, ImreadModes.Color);
+        string path = DataPath.ImagesOutput;
+        
+        int index = 0;
+        foreach (Rect face in faces) {
+            var faceImage = src.SubMat(face);
+            string number = index.ToString(); 
+            Cv2.ImWrite(path + number + ".jpg", faceImage);
+            index++; 
+        }
+    }
+
+
+    private Mat FindFace(params CascadeClassifier[] cascade)
     {
         Mat result;
 
-        using (var src = new Mat(DataPath.Yalta, ImreadModes.Color))
+        using (var src = new Mat(DataPath.PeopleGrid, ImreadModes.Color))
         using (var gray = new Mat())
         {
             result = src.Clone();
             Cv2.CvtColor(src, gray, ColorConversionCodes.BGR2GRAY);
 
             // Detect faces
-            Rect[] faces = cascade.DetectMultiScale(
-                gray, 1.08, 2, HaarDetectionTypes.ScaleImage, new Size(30, 30));
+            Rect[] faces = GetFaces(cascade);
 
             // Render all detected faces
             foreach (Rect face in faces)
