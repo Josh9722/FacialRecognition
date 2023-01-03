84d5ecd38b8912d818d5bc4a54e1f592401b9f2e
Josh9722 <wellsy9722@gmail.com>
2023-01-03 15:33:17 +1100

    Implementation of utilities class

--- Utilities.cs
+++ Utilities.cs
@@ -1,16 +1,46 @@
 using OpenCvSharp;
 
 
-public static class Utilities { 
-    public static Mat Normalise(Mat src) { 
-        Mat result = src.Clone();
-        Mat gray = new Mat();
-        Cv2.CvtColor(src, gray, ColorConversionCodes.BGR2GRAY);
-        Cv2.ImShow("Gray", gray);
-        Cv2.WaitKey(3000); 
-        Cv2.EqualizeHist(gray, gray);
-        Cv2.ImShow("Equalized", gray);
-        Cv2.WaitKey(3000);
-        return gray;
+public static class Utilities
+{
+    // ************ PRIVATE CLASS VARIABLES ************
+    private static int imgnum = 0;
+
+
+    // ************ PUBLIC METHODS ************
+    public static void SaveAllFaces(Mat src, string path = "") // Save all faces in a mat to jpgs at path
+    {
+        if (src == null || src.Empty())
+        {
+            return;
+        }
+
+        Rect[] faces = new DetectFace().GetFaces(src);
+        foreach (Rect face in faces)
+        {
+            SaveFace(src, face, path);
+        }
     }
+    
+    public static void SaveFace(Mat parent, Rect face, string path = "") // Save face as jpg from dimentions in parent mat. 
+    {
+        if (face == Rect.Empty || face.Size == Size.Zero) {
+            return; 
+        } 
+        if (parent == null || parent.Empty())
+        {
+            return;
+        }
+
+        if (path == "")
+        {
+            path = DataPath.ImagesOutput; // Default output if none given
+        } 
+
+        Mat faceImage = parent.SubMat(face);
+        faceImage = ImageProcessing.PreProcess(faceImage);
+        Cv2.ImWrite(path + imgnum.ToString() + ".jpg", faceImage);
+        imgnum++;
+    }
+
 }
