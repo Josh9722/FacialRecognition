0ca92686df94922188f7b9ee82eef91c46b758de
Josh9722 <wellsy9722@gmail.com>
2023-01-03 15:33:51 +1100

    Implementation of ImageProcessing class

--- ImageProcessing.cs
+++ ImageProcessing.cs
@@ -1,4 +1,26 @@
-class ImageProcessing { 
+using OpenCvSharp;
+
+static class ImageProcessing { 
     // Pre Processing to increase accuracy of confidence values
+
+    public static List<Mat> GetAllVersions(Mat src) { 
+        List<Mat> alternatives = new List<Mat>();
+        alternatives.Add(src);
+        alternatives.Add(Flipped(src));
+        return alternatives; 
+    }
+    
+    public static Mat PreProcess(Mat src) { // Should be run on Mat's containing face only
+        Mat grey = src.Clone();
+        Cv2.CvtColor(src, grey, ColorConversionCodes.BGR2GRAY);
+        Mat result = grey.Resize(new Size(200, 200));
+        return result;
+    }
+
+    public static Mat Flipped(Mat src) { 
+        Mat result = src.Clone();
+        Cv2.Flip(src, result, FlipMode.X);
+        return result;
+    }
 
 }
