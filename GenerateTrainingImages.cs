554a6acf1b0cb2f67dce1938ab77c82a06ae3e3f
Josh9722 <wellsy9722@gmail.com>
2023-01-03 15:35:05 +1100

    Implementation of GenerateTrainingImages class

--- GenerateTrainingImages.cs
+++ GenerateTrainingImages.cs
@@ -1,6 +1,41 @@
-class GenerateTrainingImages { 
-    public GenerateTrainingImages() { 
-        
+using OpenCvSharp;
 
+class GenerateTrainingImages { 
+    // ************ PUBLIC CLASS VARIABLES ************
+    public static bool generatingImages = true;
+
+
+    // ************ PRIVATE CLASS VARIABLES ************
+    private static string OutputPath = DataPath.TrainingRecognitionImages;
+    private CameraFeed feed; 
+    private Mat lastFrame; 
+
+
+    // ************ PUBLIC METHODS ************
+    public GenerateTrainingImages()
+    {
+        feed = new CameraFeed();
+        lastFrame = feed.facesFrame; 
+        GenerationLoop();
+    }
+
+    public void AddSample(Mat Image) {
+        Utilities.SaveAllFaces(Image, OutputPath);
+    }
+
+
+    // ************ PRIVATE METHODS ************
+    private void GenerationLoop()
+    {
+        while (generatingImages && feed.reading)
+        {
+            TakeNewSample();
+        }
+    }
+    
+    private void TakeNewSample() {
+        if (feed.frameReady && feed.facesFrame != lastFrame) { 
+            Utilities.SaveAllFaces(feed.facesFrame, DataPath.ImagesOutput);
+        }
     }
 }
